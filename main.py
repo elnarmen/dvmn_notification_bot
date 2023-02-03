e03266a943f158031943af067ad9619ba22329bb
Elnar Mengelbaev <elnar_men@mail.ru>
2023-02-03 16:32:09 +0300

    send logs to Telegram

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import os
 import time
+import logging
 from dotenv import load_dotenv
 import requests
 import telegram
+
+
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, bot, chat_id):
+        super().__init__()
+        self.bot = bot
+        self.chat_id = chat_id
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def send_message(bot, chat_id, lesson_title, is_negative_result, lesson_url):
@@ -23,21 +35,31 @@ def send_message(bot, chat_id, lesson_title, is_negative_result, lesson_url):
 
 def main():
     load_dotenv()
+
     dvmn_token = os.getenv('DVMN_TOKEN')
     telegram_token = os.getenv('TELEGRAM_TOKEN')
     chat_id = os.getenv('CHAT_ID')
-    bot = telegram.Bot(token=telegram_token)
+
     url = 'https://dvmn.org/api/long_polling/'
     params = {'timestamp': ''}
     headers = {'Authorization': f'Token {dvmn_token}'}
     waiting_time = 30
+
+    bot = telegram.Bot(token=telegram_token)
+
+    logger = logging.getLogger("bot_logger")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogsHandler(bot, chat_id))
+    logger.info("Бот запущен")
+
     while True:
         try:
             response = requests.get(url, params=params, headers=headers)
             waiting_time = 30 # время ожидания в секундах перед запросом в случае проблем с сетью
         except requests.exceptions.ReadTimeout:
             continue
-        except ConnectionError:
+        except ConnectionError as err:
+            logger.error(err)
             time.sleep(waiting_time)
             if waiting_time < 7200:
                 waiting_time *= 1.2
